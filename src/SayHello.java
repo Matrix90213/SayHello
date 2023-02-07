dc90d5b1632a05258705b7b3a9386a6c9a6dd737
Ian Pillado <22670007@iguala.tecnm.mx>
2023-02-07 07:50:04 -0600

    Cambie el mensaje de Say Hello a Hello

--- src/SayHello.java
+++ src/SayHello.java
@@ -1,7 +1,7 @@
 public class SayHello {
     public static void main(String[] args) {
         String name="Ian";
-        System.out.println("Say Hello: " + name);
+        System.out.println("Hello " + name);
 
     }
 }
