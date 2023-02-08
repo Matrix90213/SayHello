effbf7e5a2c0ee808ebae93bda19607a81a6cbb6
Ian Pillado <22670007@iguala.tecnm.mx>
2023-02-08 07:18:48 -0600

    Practica 1 terminada - IanPillado

---
+++ src/Suma.java
@@ -0,0 +1,11 @@
+public class Suma {
+    public static void main(String[] args) {
+        int num1=2;
+        int num2=2;
+        int suma;
+        suma=num1+num2;
+        System.out.println("La suma es "+suma);
+
+    }
+
+}
