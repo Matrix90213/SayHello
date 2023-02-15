09ef4038428a01db8de76e93255eef243133b48f
Ian Pillado <22670007@iguala.tecnm.mx>
2023-02-14 22:20:16 -0600

    Se crea clase Animal

---
+++ src/Animal.java
@@ -0,0 +1,7 @@
+public class Animal {
+    private String group;  //mamieferos,aves,peces,reptiles y anfibios
+    private String specie;  //Ballena,Vibora,León,Anaconda
+    private String genre;  //Panthera,Eunectes
+    private String habitat;  //Acuaticos,Semiacuaticos,Terrestres,Aeroterrestres
+    private String lenght;  //Alto cms
+}
